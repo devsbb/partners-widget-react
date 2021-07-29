475c4e3c49faafb096497f77ff65a4ec2910c31a
IlitA <imilitamar@gmail.com>
2021-07-29 20:32:31 +0200

    feat: new widget style

--- src/translations/dictionaries/de.js
+++ src/translations/dictionaries/de.js
@@ -1,10 +1,10 @@
 const de = {
-    RENT_NOW_HEADER: 'Jetzt mieten statt kaufen',
-    RENT_WITH_GROVER_BUTTON: 'Mieten mit Grover',
+    RENT_NOW_HEADER: 'Inklusive Schadenschutz',
+    RENT_WITH_GROVER_BUTTON: 'Weiter mit Grover',
     MORE_INFO_LINK: 'Mehr Infos',
     UNAVAILABLE_BUTTON: 'Bei Grover derzeit nicht verfügbar',
     STARTING_PRICE: 'ab',
-    PERIODICITY: 'pro Monat',
+    PERIODICITY: '/ Monat flexibel mieten',
 };
 
 export default de;
