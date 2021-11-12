3d588114b70dc079d4403624f99205cc4d3037f6
IlitA <16757443+IlitA@users.noreply.github.com>
2021-11-12 12:03:06 +0100

    feat: update widget for mediamarkt (#30)

--- src/translations/dictionaries/de.js
+++ src/translations/dictionaries/de.js
@@ -1,10 +1,10 @@
 const de = {
-    RENT_NOW_HEADER: 'Inklusive Schadenschutz',
-    RENT_WITH_GROVER_BUTTON: 'Jetzt mit Grover mieten',
+    RENT_NOW_HEADER: 'Zusätzlich 99 % im 1. Monat sparen',
+    RENT_WITH_GROVER_BUTTON: 'Jetzt flexibel mieten',
     MORE_INFO_LINK: 'Mehr Infos',
     UNAVAILABLE_BUTTON: 'Bei Grover derzeit nicht verfügbar',
     STARTING_PRICE: 'ab',
-    PERIODICITY: '/ Monat mieten',
+    PERIODICITY: '/ Monat flexibel mieten',
 };
 
 export default de;
