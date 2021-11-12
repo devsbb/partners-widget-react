ea72249effec22ce93d00513fcd129578b864195
IlitA <imilitamar@gmail.com>
2021-11-12 11:34:27 +0100

    feat: update widget for Unito

--- src/translations/dictionaries/de.js
+++ src/translations/dictionaries/de.js
@@ -1,10 +1,10 @@
 const de = {
-    RENT_NOW_HEADER: 'Zusätzlich 99 % im 1. Monat sparen',
-    RENT_WITH_GROVER_BUTTON: 'Jetzt flexibel mieten',
+    RENT_NOW_HEADER: 'Inklusive Schadenschutz',
+    RENT_WITH_GROVER_BUTTON: 'Jetzt mit Grover mieten',
     MORE_INFO_LINK: 'Mehr Infos',
     UNAVAILABLE_BUTTON: 'Bei Grover derzeit nicht verfügbar',
     STARTING_PRICE: 'ab',
-    PERIODICITY: '/ Monat flexibel mieten',
+    PERIODICITY: '/ Monat mieten',
 };
 
 export default de;
